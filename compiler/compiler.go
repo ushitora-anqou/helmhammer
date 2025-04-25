22104de63cbe668ef27aa45e2f7c6891edf21348
Ushitora Anqou <ushitora@anqou.net>
2025-04-25 23:02:32 +0900

    wip

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -11,61 +11,6 @@ import (
 	"github.com/ushitora-anqou/helmhammer/jsonnet"
 )
 
-var nextGenID = 0
-
-func genid() int {
-	nextGenID++
-	return nextGenID
-}
-
-// state is output of execution of a text/template's Node.
-// It has a printed value (`v`) and a variable map `vs` (name |-> value)
-// after its execution.
-type state struct {
-	body *jsonnet.Expr
-}
-
-func newState(v *jsonnet.Expr, vs *jsonnet.Expr) *state {
-	return &state{
-		body: &jsonnet.Expr{
-			Kind: jsonnet.EMap,
-			Map: map[*jsonnet.Expr]*jsonnet.Expr{
-				stringLiteralStateV:  v,
-				stringLiteralStateVS: vs,
-			},
-		},
-	}
-}
-
-func (s *state) toLocal(stateName stateName, localBody *jsonnet.Expr) *jsonnet.Expr {
-	return &jsonnet.Expr{
-		Kind: jsonnet.ELocal,
-		LocalBinds: []*jsonnet.LocalBind{
-			{
-				Name: stateName,
-				Body: s.body,
-			},
-		},
-		LocalBody: localBody,
-	}
-}
-
-const (
-	stateV  = "v"
-	stateVS = "vs"
-)
-
-var (
-	stringLiteralStateV  = &jsonnet.Expr{Kind: jsonnet.EStringLiteral, StringLiteral: stateV}
-	stringLiteralStateVS = &jsonnet.Expr{Kind: jsonnet.EStringLiteral, StringLiteral: stateVS}
-)
-
-type stateName = string
-
-func generateStateName() stateName {
-	return fmt.Sprintf("s%d", genid())
-}
-
 type scope struct {
 	parent    *scope
 	variables map[string]*variable
