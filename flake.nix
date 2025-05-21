a54a21c3e64fb1e204ef83fc704aea0c648de9d0
Ushitora Anqou <ushitora@anqou.net>
2025-05-21 22:14:48 +0900

    flake.nix: use go-jsonnet v0.21.0

--- flake.nix
+++ flake.nix
@@ -13,16 +13,7 @@
     pkgs = nixpkgs.legacyPackages."${system}";
     lib = nixpkgs.lib;
 
-    #go-jsonnet = pkgs.go-jsonnet;
-    go-jsonnet = pkgs.go-jsonnet.overrideAttrs (finalAttrs: previousAttrs: {
-      src = pkgs.fetchFromGitHub {
-        owner = "google";
-        repo = "go-jsonnet";
-        rev = "2a3f4afd6af061d8b22a01e878184b04e42ca011";
-        sha256 = "sha256-Tquk8idbUYFEKLpnKRYY8cV0YgTOdti7mT9TUnu4Kx0=";
-      };
-      vendorHash = "sha256-Uh2rAXdye9QmmZuEqx1qeokE9Z9domyHsSFlU7YZsZw=";
-    });
+    go-jsonnet = pkgs.go-jsonnet;
     #jrsonnet = pkgs.jrsonnet.overrideAttrs (finalAttrs: previousAttrs: {
     #  src = pkgs.fetchFromGitHub {
     #    owner = "CertainLach";
